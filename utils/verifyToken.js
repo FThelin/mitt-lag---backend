24ef21faaf5acb9ba06e4c8d9c042220b4acdf35
FThelin <fredrik@ixlayer.com>
2020-12-01 15:25:32 +0100

    changes to verification

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -16,6 +16,7 @@ exports.verifyToken = (req, res, next) => {
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET)
         req.user = verified
+        next();
     } catch(err) {
         res.status(400).send("invalid token")
     }
