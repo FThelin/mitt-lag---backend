d9387fd130650210dd67d9fe04dbb4273e86bd3f
Fredrik Thelin <fredrikthelin82@gmail.com>
2020-12-23 16:12:57 +0100

    create team done

--- resources/team/controller.js
+++ resources/team/controller.js
@@ -2,29 +2,25 @@ const Team = require("./model");
 const User = require("../user/model");
 
 exports.createTeam = async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-    if (!user) {
-      return res.status(400).send("Something went wrong");
-    }
-
-    const { name, city, sport } = req.body;
-
-    // Create team
-    const team = await Team.create({
-      name,
-      city,
-      sport,
-      leaders: [req.user._id],
-    });
-
-    user.activeTeam = team._id;
-    user.team = [...user.team, team._id];
-    user.save();
-
-    res.status(201).json(team);
-  } catch (error) {
-    res.status(403).json(error);
+  if (!user) {
+    return res.status(400).send("Something went wrong");
   }
+
+  const { name, city, sport } = req.body;
+
+  // Create team
+  const team = await Team.create({
+    name,
+    city,
+    sport,
+    leaders: [req.user._id],
+  });
+
+  user.activeTeam = team._id;
+  user.team = [...user.team, team._id];
+  user.save();
+
+  res.status(201).json(team);
 };
