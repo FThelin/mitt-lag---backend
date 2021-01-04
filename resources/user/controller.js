09d56ef4a6cb2b1dbd35ff11e98fc19123cf82aa
FThelin <fredrik@ixlayer.com>
2021-01-04 16:36:33 +0100

    auth

--- resources/user/controller.js
+++ resources/user/controller.js
@@ -3,19 +3,23 @@ const User = require("./model");
 
 //Register new user
 exports.register = async (req, res) => {
-  const { firstname, lastname, email } = req.body;
+  try {
+    const { firstname, lastname, email } = req.body;
 
-  const password = await bcrypt.hash(req.body.password, 10);
+    const password = await bcrypt.hash(req.body.password, 10);
 
-  // Create user
-  const user = await User.create({
-    firstname,
-    lastname,
-    email,
-    password,
-  });
+    // Create user
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+    });
 
-  res.status(201).json(user);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 exports.getAllUsers = async (req, res) => {
