5a92c192ee222fa6d8d8e96c7c2cb5b6719e45b4
FThelin <fredrik@ixlayer.com>
2021-01-06 12:40:59 +0100

    request handling

--- resources/team/controller.js
+++ resources/team/controller.js
@@ -102,36 +102,32 @@ exports.deleteLeaderFromTeam = async (req, res) => {
 };
 
 exports.acceptRequest = async (req, res) => {
-  try {
-    const team = await Team.findById(req.body.teamId);
-    const request = await Request.findById(req.body.requestId);
-    const user = await User.findById(request.playerId);
+  const team = await Team.findById(req.body.teamId);
+  const request = await Request.findById(req.body.requestId);
+  const user = await User.findById(request.playerId);
 
-    if (!team) {
-      return res.status(400).send("Something went wrong with team");
-    }
-    if (!request) {
-      return res.status(400).send("Something went wrong with request");
-    }
-    if (!user) {
-      return res.status(400).send("Something went wrong with user");
-    }
-
-    user.team = [...user.team, team._id];
-
-    const index = team.requests.indexOf(request._id);
-    team.requests.splice(index, 1);
-
-    team.players = [...team.players, request.playerId];
-
-    await team.save();
-    await user.save();
-    request.remove();
-    res.status(201).json({
-      success: true,
-      data: team._id,
-    });
-  } catch (err) {
-    res.status(400).json(err);
+  if (!team) {
+    return res.status(400).send("Something went wrong with team");
   }
+  if (!request) {
+    return res.status(400).send("Something went wrong with request");
+  }
+  if (!user) {
+    return res.status(400).send("Something went wrong with user");
+  }
+
+  user.team = [...user.team, team._id];
+
+  const index = team.requests.indexOf(request._id);
+  team.requests.splice(index, 1);
+
+  team.players = [...team.players, request.playerId];
+
+  await team.save();
+  await user.save();
+  request.remove();
+  res.status(201).json({
+    success: true,
+    data: team._id,
+  });
 };
