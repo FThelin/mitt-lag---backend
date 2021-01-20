2538860ee15289c10c132ccefe05ddc402222934
Simon Beijer <beijer.simon@gmail.com>
2021-01-20 15:52:13 +0100

    Added email to errorHandler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,12 +1,14 @@
 module.exports = {
   errorMessage: function (body) {
-    const { firstname, lastname, password } = body;
+    const { firstname, lastname, password, email } = body;
     let errorMsg = "";
 
     if (firstname.length < 2) {
       errorMsg = "Förnamn saknas";
     } else if (lastname.length < 2) {
       errorMsg = "Efternamn saknas";
+    } else if (email.length < 3) {
+      errorMsg = "email saknas exempel@exempel.se";
     } else if (password.length < 6) {
       errorMsg = "Lösenord för kort";
     }
